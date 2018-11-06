276535da3a8bd774ba597b4dd9bfc2036a7f48e0
qwerjunety <junemelodelachica@gmail.com>
2018-11-06 22:01:51 +0800

    second commit

---
+++ app/src/main/java/june/delachica/com/sai/ForgotPassSuccess.java
@@ -0,0 +1,18 @@
+package june.delachica.com.sai;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ForgotPassSuccess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_pass_success);
+
+    }
+}
--- app/src/main/java/june/delachica/com/sai/ForgotPassword.java
+++ app/src/main/java/june/delachica/com/sai/ForgotPassword.java
@@ -1,7 +1,10 @@
 package june.delachica.com.sai;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ForgotPassword extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class ForgotPassword extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        Button sendForgot = findViewById(R.id.sendForgot);
+        sendForgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ForgotPassword.this, ForgotPassSuccess.class);
+                startActivity(i);
+            }
+        });
     }
 }
